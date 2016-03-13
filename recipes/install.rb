c06fbfacec3c73e9806327e789fd019781d4d6fb
vjdhama <vjdhama26@gmail.com>
2016-03-13 21:02:54 +0530

    installation step added using apt package

--- recipes/install.rb
+++ recipes/install.rb
@@ -2,4 +2,7 @@
 # Cookbook Name:: crystal
 # Recipe:: install
 #
+apt_package 'crystal' do
+    action :install
+end
 
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,3 +5,7 @@ describe file('/etc/apt/sources.list.d/crystal.list') do
   it { should contain "deb http://dist.crystal-lang.org/apt crystal main" }
 end
 
+describe package('crystal') do
+    it { should be_installed }
+end
+
