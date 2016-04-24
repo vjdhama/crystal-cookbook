d2c61a5c9baede2d2e9f2e1350c175cb09d2b60e
vjdhama <vjdhama26@gmail.com>
2016-04-24 21:02:03 +0530

    removed debian and centos minor version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'vjdhama26@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures crystal'
 long_description 'Cookbook for installing crystal'
-version          '0.2.0'
+version          '0.2.1'
 
