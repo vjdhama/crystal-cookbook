18e3e4b24660aafc39b6d12ca566606097d70c82
Alexander Koch <c-alexander.koch@charter.com>
2019-01-30 15:22:26 -0700

    version bump and removes mojave box

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'vjdhama26@gmail.com'
 license          'MIT'
 description      'Installs/Configures crystal'
 long_description 'Cookbook for installing crystal'
-version          '0.2.2'
+version          '0.2.3'
 
 depends 'homebrew'
