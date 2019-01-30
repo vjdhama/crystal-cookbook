f602fe53fd4bd8cddf175ade72b50eb8932d0a18
Alexander Koch <c-alexander.koch@charter.com>
2019-01-29 17:56:36 -0700

    adds license info to metadata.rb and finishes support for InSpec

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,7 @@
 name             'crystal'
 maintainer       'Vijay Dhama'
 maintainer_email 'vjdhama26@gmail.com'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs/Configures crystal'
 long_description 'Cookbook for installing crystal'
 version          '0.2.2'
-
